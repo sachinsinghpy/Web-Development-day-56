fd19a383efec573f905b2c3d14cd22d3dabf7671
Sachin Kumar <skraja012345678@gmail.com>
2025-07-28 19:42:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello I am coditional tutorial")
+
+let age = 45;
+if (age>18){
+    console.log("You can drive");
+}
+else if (age == 0){
+    console.log("Are You kidding!")
+}
+else{
+    console.log("You cannot drive")
+}
+
+let a = 5
+let b = 9
+
+let c = a>b ? (a-b):(b-a)
+console.log(c)
